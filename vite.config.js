1190047c53945975ad44499ae50bff0b721c77f6
Mauricio Espinoza P <espinoza.p.mauricio@gmail.com>
2023-10-07 11:22:17 -0300

    FrontEnd: update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    define: {
-      __APP_ENV__: env.APP_ENV
-    },
-    plugins: [react()],
-    server: {
-      port: 4010
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
+// https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '')
+
+//   return {
+//     define: {
+//       __APP_ENV__: env.APP_ENV
+//     },
+//     plugins: [react()],
+//     server: {
+//       port: 4010
+//     }
+//   }
+// })
